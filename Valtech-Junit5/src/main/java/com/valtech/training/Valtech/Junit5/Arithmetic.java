94a148ac647e13dcd7c77fcbc0f199a813f12fdc
Shruthi D <Shruthi.D@valtech.com>
2022-10-13 15:48:07 +0530

    junit5

---
+++ Valtech-Junit5/src/main/java/com/valtech/training/Valtech/Junit5/Arithmetic.java
@@ -0,0 +1,13 @@
+package com.valtech.training.Valtech.Junit5;
+
+public interface Arithmetic {
+	
+	public int add(int a,int b);
+	
+	public int sub(int a,int b);
+	
+	public int mul(int a,int b);
+	
+	public int div(int a,int b);
+	
+}
---
+++ Valtech-Junit5/src/main/java/com/valtech/training/Valtech/Junit5/ArithmeticImpl.java
@@ -0,0 +1,24 @@
+package com.valtech.training.Valtech.Junit5;
+
+public class ArithmeticImpl implements Arithmetic {
+public int add(int a, int b) {
+		
+		return a+b;
+	}
+
+	public int sub(int a, int b) {
+	
+		return a-b;
+	}
+
+	public int mul(int a, int b) {
+	
+		return a*b;
+	}
+
+	public int div(int a, int b) {
+	
+		return a/b;
+	}
+
+}
